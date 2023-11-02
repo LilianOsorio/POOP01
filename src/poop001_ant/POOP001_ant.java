819f1f4d95b6d475af263417d7b5a7bd40fc794f
LILIAN OSORIO <146885696+LilianOsorio@users.noreply.github.com>
2023-11-02 00:11:31 -0600

    Creación de practica 01 con Ant

---
+++ src/poop001_ant/POOP001_ant.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package poop001_ant;
+
+/**
+ *La clase POOP001_ant es la clase principal que contiene el método main.
+ *Este método imprime un mensaje de saludo en la consola.
+ * @author lilian
+ */
+public class POOP001_ant {
+    /**
+     * El método main es el punto de entrada principal para la aplicación.
+     * Aquí se imprime un mensaje en la consola.
+     *
+     * @param args Los argumentos de la línea de comandos (no se utilizan en este ejemplo).
+     */
+    public static void main(String[] args) {
+        // Imprime un mensaje de saludo en la consola
+        System.out.println("Hola mundo desde POO grupo08 con Ant");
+    }
+    
+}
